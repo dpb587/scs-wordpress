a12885694a423b0c6779a4ff40fffadc983d9a62
Danny Berger <dpb587@gmail.com>
2014-01-28 14:15:26 -0700

    liveupdate test; supervisor:9001

--- scs/bin/update-requires-mysql.sh
+++ scs/bin/update-requires-mysql.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# args: source
+
+set -e
+
 sed -i "s/env\[SCS_REQUIRES_MYSQL\] = \(.*\)/env[SCS_REQUIRES_MYSQL] = $SCS_REQUIRES_MYSQL/" /scs/etc/php-fpm/php-fpm.ini
+
+if [ "run" != "$1" ]; then
+    supervisorctl -c /scs/etc/supervisor.conf restart php-fpm
+fi
