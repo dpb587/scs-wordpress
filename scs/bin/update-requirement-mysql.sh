de6f15e1dd85106f534afe593934aa9f8dd96f43
Danny Berger <dpb587@gmail.com>
2014-02-16 20:40:58 -0700

    change naming

--- scs/bin/update-requirement-mysql.sh
+++ scs/bin/update-requirement-mysql.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-sed -i "s/env\[SCS_REQUIRES_MYSQL\] = \(.*\)/env[SCS_REQUIRES_MYSQL] = $SCS_REQUIRES_MYSQL/" /scs/etc/php-fpm/php-fpm.ini
+sed -i "s/env\[SCS_REQUIRE_MYSQL\] = \(.*\)/env[SCS_REQUIRE_MYSQL] = $SCS_REQUIRE_MYSQL/" /scs/etc/php-fpm/php-fpm.ini
 
 if [ "run" != "$1" ]; then
     supervisorctl -c /scs/etc/supervisor.conf restart php-fpm
